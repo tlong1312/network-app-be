c0ab502961721bda07df5640fe9374853e8d12de
Dang Tieu Long <dangtieulong362@gmail.com>
2025-04-29 21:42:22 +0700

    Sửa FriendRepository

--- src/main/java/com/mhpl/network_app_backend/repository/FriendRepository.java
+++ src/main/java/com/mhpl/network_app_backend/repository/FriendRepository.java
@@ -1,5 +1,7 @@
 package com.mhpl.network_app_backend.repository;
 
+import com.mhpl.network_app_backend.entity.Friend;
+import com.mhpl.network_app_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -7,13 +9,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-public interface FriendRepository extends JpaRepository<com.mhpl.network_app_backend.entity.Friend, Integer> {
+public interface FriendRepository extends JpaRepository<Friend, Integer> {
 
     boolean existsByUserIdAndFriendId(int userId, int friendId);
 
-    Optional<com.mhpl.network_app_backend.entity.Friend> findByUserIdAndFriendId(int userId, int friendId);
+    Optional<Friend> findByUserIdAndFriendId(int userId, int friendId);
 
     @Query("SELECT u FROM User u JOIN Friend f ON (f.userId = :userId OR f.friendId = :userId) AND f.status = 'accepted'")
-    List<com.mhpl.network_app_backend.entity.User> findAcceptedFriends(@Param("userId") int userId);
+    List<User> findAcceptedFriends(@Param("userId") int userId);
 
 }
